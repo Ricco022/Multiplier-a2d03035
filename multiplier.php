4ced8086f23cdf4b866ea361bd13d7fe337e6007
Riccardo van der Horst <riccardo271002@gmail.com>
2019-11-15 13:54:00 +0100

    Uitwerking van de exercise

---
+++ multiplier.php
@@ -0,0 +1,12 @@
+<?php
+
+echo 'Ik ga twee getallen met elkaar vermenigvuldigen'.PHP_EOL;
+
+$a = Readline ("Eerste getal?");
+
+$b = Readline ("Tweede getal?");
+
+echo ("Uw resultaat is: ".$a*$b);
+
+
+?>
